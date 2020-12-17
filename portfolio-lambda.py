77cf13e23678415f7bdca8fb76d7feb6a531abb7
Manuel Duarte <duartemanuel@hotmail.com>
2020-12-17 11:24:38 -0800

    Fixed the lambda fuction and delete upload-portfolio-lambda

--- portfolio-lambda.py
+++ portfolio-lambda.py
@@ -1,17 +1,30 @@
 import boto3
-from botocore.client import Config
 import zipfile
+import io
+from io import BytesIO
+import mimetypes
 
-s3 = boto3.resource('s3', config=Config(signature_version='s3v4'))
+s3 = boto3.resource('s3')
 
-build_bucket = s3.Bucket('portfoliobuild.robinnorwood.info')
-portfolio_bucket = s3.Bucket('portfolio.robinnorwood.info')
+portfolio_bucket = s3.Bucket('portfolio.ithandymanny.net')
 
-# On Windows, this will need to be a different location than /tmp
-build_bucket.download_file('portfolio.zip', '/tmp/portfolio.zip')
+for obj in portfolio_bucket.objects.all():
+        print(obj.key)
+        
+build_bucket = s3.Bucket('portfoliobuild.ithandymanny.net')
 
-with zipfile.ZipFile('/tmp/portfolio.zip') as myzip:
+build_bucket.download_file('buildportfolio', '/home/fitz2020/Downloads/buildportfolio')
+
+
+
+buildportfolio = BytesIO()
+
+build_bucket.download_fileobj('buildportfolio',buildportfolio)
+
+        
+with zipfile.ZipFile(buildportfolio) as myzip:
     for nm in myzip.namelist():
-        obj = myzip.open(nm)
-        target_bucket.upload_fileobj(obj, nm)
-        target_bucket.Object(nm).Acl().put(ACL='public-read')
+        obj= myzip.open(nm)
+        portfolio_bucket.upload_fileobj(obj, nm,
+          ExtraArgs={'ContentType': mimetypes.guess_type(nm) [0]})
+        portfolio_bucket.Object(nm).Acl().put(ACL='public-read')
